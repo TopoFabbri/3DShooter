3bc58367787f02d519bec1d37bb201833147ae52
TopoArania <matufabbri@gmail.com>
2023-06-29 17:47:15 -0300

    Fixed 'Explosion' TODOs

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,23 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class Explosion : MonoBehaviour
 {
-    private ParticleSystem ps;
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnParticleSystemStopped()
     {
-        ps = GetComponent<ParticleSystem>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //TODO: FIX -Use particle system callback
-        if (!ps.isPlaying)
-        {
-            Destroy(gameObject);
-        }
+        Destroy(gameObject);
     }
 }
